4d4b97eb2343f54a3fa6537da123919a53efabee
basil-kawak <basilalisa0@gmail.com>
2020-09-10 23:26:55 +0300

    Updates

---
+++ precache-manifest.5f09dcbe431b7a15d1f5fe761041ef6d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2536aa2acb3590d7dd90b459f3ccbd2b",
+    "url": "/basil-kawak/portfolio/index.html"
+  },
+  {
+    "revision": "a1e7b9c0d81201edafc4",
+    "url": "/basil-kawak/portfolio/static/css/2.18bc7be3.chunk.css"
+  },
+  {
+    "revision": "593216416e0a14b68921",
+    "url": "/basil-kawak/portfolio/static/css/main.27e302f9.chunk.css"
+  },
+  {
+    "revision": "a1e7b9c0d81201edafc4",
+    "url": "/basil-kawak/portfolio/static/js/2.ba8aeb31.chunk.js"
+  },
+  {
+    "revision": "a986d777e7ab357b3f79ca1eede38fb5",
+    "url": "/basil-kawak/portfolio/static/js/2.ba8aeb31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "593216416e0a14b68921",
+    "url": "/basil-kawak/portfolio/static/js/main.31336dff.chunk.js"
+  },
+  {
+    "revision": "e7fb13598dbf04e14fd6",
+    "url": "/basil-kawak/portfolio/static/js/runtime-main.c8e8f9a6.js"
+  },
+  {
+    "revision": "794179572ef46d7f9a3fb426850b285a",
+    "url": "/basil-kawak/portfolio/static/media/Basil M. Al-Isa.79417957.pdf"
+  },
+  {
+    "revision": "5af31e049fb7d606a49ad788e5ec602e",
+    "url": "/basil-kawak/portfolio/static/media/Wsignature.5af31e04.png"
+  },
+  {
+    "revision": "8d61f0c29fa07b0fd2d1a223c7d6a7f7",
+    "url": "/basil-kawak/portfolio/static/media/project1.8d61f0c2.png"
+  },
+  {
+    "revision": "f1e2ebf6099c9232e5c39a5409c90f0d",
+    "url": "/basil-kawak/portfolio/static/media/project2.f1e2ebf6.png"
+  },
+  {
+    "revision": "d72faf3148a0d76e8268288362212364",
+    "url": "/basil-kawak/portfolio/static/media/project3.d72faf31.png"
+  },
+  {
+    "revision": "426d7a040be86ffd417d4e64220ac198",
+    "url": "/basil-kawak/portfolio/static/media/project4.426d7a04.png"
+  },
+  {
+    "revision": "b9306fc38d65b204c40be02170285c35",
+    "url": "/basil-kawak/portfolio/static/media/project5.b9306fc3.png"
+  }
+]);
