cdcfbe97e111d834d2daf3186453efd810853f9f
basil-kawak <basilalisa0@gmail.com>
2020-09-10 23:42:51 +0300

    Updates

---
+++ precache-manifest.7f8ad02968a3e869243d1bdd4c69c257.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d0598279e4d12ea1a9eacaec9d0006",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf28d5f0ad9d2918be49",
+    "url": "/static/css/2.18bc7be3.chunk.css"
+  },
+  {
+    "revision": "58c22f2645a88505977c",
+    "url": "/static/css/main.27e302f9.chunk.css"
+  },
+  {
+    "revision": "bf28d5f0ad9d2918be49",
+    "url": "/static/js/2.92b8b43f.chunk.js"
+  },
+  {
+    "revision": "a986d777e7ab357b3f79ca1eede38fb5",
+    "url": "/static/js/2.92b8b43f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c22f2645a88505977c",
+    "url": "/static/js/main.7df8b83e.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "794179572ef46d7f9a3fb426850b285a",
+    "url": "/static/media/Basil M. Al-Isa.79417957.pdf"
+  },
+  {
+    "revision": "5af31e049fb7d606a49ad788e5ec602e",
+    "url": "/static/media/Wsignature.5af31e04.png"
+  },
+  {
+    "revision": "8d61f0c29fa07b0fd2d1a223c7d6a7f7",
+    "url": "/static/media/project1.8d61f0c2.png"
+  },
+  {
+    "revision": "f1e2ebf6099c9232e5c39a5409c90f0d",
+    "url": "/static/media/project2.f1e2ebf6.png"
+  },
+  {
+    "revision": "d72faf3148a0d76e8268288362212364",
+    "url": "/static/media/project3.d72faf31.png"
+  },
+  {
+    "revision": "426d7a040be86ffd417d4e64220ac198",
+    "url": "/static/media/project4.426d7a04.png"
+  },
+  {
+    "revision": "b9306fc38d65b204c40be02170285c35",
+    "url": "/static/media/project5.b9306fc3.png"
+  }
+]);
